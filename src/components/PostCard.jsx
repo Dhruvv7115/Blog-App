0707ac24a4493b0bad3318aac4025953844c068e
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-07-08 00:46:57 +0530

    refactor: post-card ui improved

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import blogService from '../appwrite/blog'
+import React from "react";
+import { Link } from "react-router-dom";
+import blogService from "../appwrite/blog";
 
 function PostCard({ $id, title, featuredImage }) {
-  return (
-    <Link to={`/post/${$id}`}>
-      <div className="bg-yellow-300 p-4 w-full hover:shadow-2xl duration-200 rounded-xl hover:-translate-y-2 min-h-full flex flex-col justify-around">
-        <img src={blogService.getFileView(featuredImage)} alt={title} className="w-full mb-4 rounded-xl object-cover h-64" />
-        <h1 className="text-2xl font-bold text-blue-950">{title}</h1>
-      </div>
-    </Link>
-  )
+	return (
+		<div>
+			<div className="bg-amber-300 p-1.5 w-full hover:shadow-2xl duration-200 rounded-2xl hover:-translate-y-2 min-h-full flex flex-col justify-around">
+				<img
+					src={blogService.getFileView(featuredImage)}
+					alt={title}
+					className="w-full mb-6 rounded-2xl object-cover h-64"
+				/>
+				<h1 className="text-lg font-semibold text-blue-950 mx-2">{title}</h1>
+
+				<button className="px-4 py-1 mx-2 mt-4 mb-2 bg-blue-900 rounded-full text-amber-100 w-fit text-sm cursor-pointer">
+					<Link to={`/post/${$id}`}>Explore</Link>
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default PostCard
+export default PostCard;
